b7928a7a5a103b72621522201465665c477b259b
anjinsan132131 <anjinsan132131@gmail.com>
2022-01-31 22:52:00 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let array = [];
+  let brackets = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == brackets.get(array[array.length - 1])) {
+      array.pop();
+    } else {
+      array.push(str[i]);
+    }
+  }
+
+  if(array.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
